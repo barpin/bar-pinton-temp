7950225d0f7475e36e149de4d3f290f31810e7fb
0horane <leandro.horane.siracusa@gmail.com>
2022-03-04 20:11:15 -0600

    github link

--- partials/footer.php
+++ partials/footer.php
@@ -4,11 +4,13 @@
 <footer>
     <div class="social-media-links">
       <ul class="ul-footer">
-      <li class="li-footer">
-          <!-- twitter -->
-          <a class="a-footer hover:text-gray-500">
-          <ion-icon name="logo-twitter"></ion-icon>
-          </a>
+        <li class="li-footer">
+          <!-- github -->
+          <span class="a-footer hover:text-gray-600">
+            <a href="https://github.com/0horane/cecs-html">
+              <ion-icon name="logo-github"></ion-icon>
+            </a>
+          </span>
         </li>
         <li class="li-footer">
           <!-- facebook -->
