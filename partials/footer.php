ea788bdd02a40a4a867066fee345c4b1696a5803
0horane <83721176+0horane@users.noreply.github.com>
2022-06-30 08:37:41 -0300

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -4,92 +4,31 @@
 <footer>
     <div class="social-media-links">
       <ul class="ul-footer">
+          <!--
         <li class="li-footer">
-          <!-- github -->
+           github 
           <span class="a-footer hover:text-gray-600">
             <a href="https://github.com/0horane/cecs-html">
               <ion-icon name="logo-github"></ion-icon>
             </a>
           </span>
         </li>
+-->
         <li class="li-footer">
           <!-- facebook -->
-          <span class="a-footer hover:text-gray-600">
-            <a>
+          <span class="a-footer hover:text-blue-600">
+            <a href="https://www.facebook.com/barpinton/">
               <ion-icon name="logo-facebook"></ion-icon>
             </a>
-            <div class="subicons text-blue-600">
-              <a href="https://www.facebook.com/lasuizacompu/">
-                <ion-icon name="laptop"></ion-icon>
-              </a>
-              <a href="https://www.facebook.com/tecnica26/">
-                <ion-icon name="business"></ion-icon>
-              </a>
-            </div>
           </span>
-        </li>
-        <li class="li-footer">
-          <!-- web -->
-          <span class="a-footer hover:text-blue-600">
-            <a href="#/">
-              <ion-icon name="globe"></ion-icon>
-            </a>
-            <div class="subicons subiconsmed text-blue-600">
-              <a href="http://laboratorios.et26.edu.ar">
-                <ion-icon name="laptop"></ion-icon>
-              </a>
-              <a href="#/">
-                <ion-icon name="people"></ion-icon>
-              </a>
-              <a href="https://sites.google.com/view/aulasvirtualeset26">
-                <ion-icon name="business"></ion-icon>
-              </a>
-            </div>
-          </span>
-        </li>     
+        </li>    
         <li class="li-footer">
           <!-- instagram -->
           <span class="a-footer hover:text-blue-600">
-            <a href="https://www.instagram.com/c.e.c.s_/">
+            <a href="https://www.instagram.com/barpinton/">
               <ion-icon name="logo-instagram"></ion-icon>
             </a>
-            <div class="subicons subiconslong text-blue-600">
-              <a href="https://www.instagram.com/lasuizacompu/">
-                <ion-icon name="laptop"></ion-icon>
-              </a>
-              <a href="https://www.instagram.com/tecnica26ok/">
-                <ion-icon name="business"></ion-icon>
-              </a>
-              <a href="https://www.instagram.com/c.e.c.s_/">
-                <ion-icon name="people"></ion-icon>
-              </a>
-              <a href="https://www.instagram.com/la26automotores/">
-                <ion-icon name="car"></ion-icon>
-              </a>
-            </div>
           </span>
-        </li>
-        <li class="li-footer">
-          <!-- youtube -->
-          <span class="a-footer hover:text-gray-600">
-            <a>
-              <ion-icon name="logo-youtube"></ion-icon>
-            </a>
-            <div class="subicons text-blue-600">
-              <a href="https://www.youtube.com/channel/UCTKTGa96qO3ESC1t2JqzfzQ/">
-                <ion-icon name="laptop"></ion-icon>
-              </a>
-              <a href="https://www.youtube.com/c/tecnica26/">
-                <ion-icon name="business"></ion-icon>
-              </a>
-            </div>
-          </span>
-        </li>            
-        <li class="li-footer">
-          <!-- discord -->
-          <a class="a-footer hover:text-gray-500">
-          <ion-icon name="logo-discord"></ion-icon>
-          </a>
         </li>
       </ul>
     </div>
